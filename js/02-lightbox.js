e8f5d45a38c08bf3b11ceafafa83d8e4530e47bd
SerhiiSamara <ndaair@gmail.com>
2022-09-27 01:42:16 +0300

    Зробив

---
+++ js/02-lightbox.js
@@ -0,0 +1,29 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+creatGalleryMarkup(galleryMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `		
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join("");
+}
+
+function creatGalleryMarkup(galleryMarkup) {
+  galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
